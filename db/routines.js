ee2eca849c7700656b255e742e46d51074721eda
Remics89 <Remics89@gmail.com>
2022-12-10 09:40:04 -0800

    moved queries to try/catch blocks

--- db/routines.js
+++ db/routines.js
@@ -1,25 +1,39 @@
 const client = require('./client');
 
 async function getRoutineById(id){
-  const {row: [routine]} = await client.query(`
+  try {
+    const {row: [routine]} = await client.query(`
     SELECT * 
     FROM routines
     WHERE id=$1;
   ` [id]);
 
   return routine;
+
+  } catch (error) {
+    console.log("Could not get routine by ID")
+    throw error;
+  }
+  
 }
 
 async function getRoutinesWithoutActivities(){
 }
 
 async function getAllRoutines() {
-  const routines = await client.query(`
+  try {
+    const routines = await client.query(`
     SELECT *
     FROM routines;
   `)
 
   return routines;
+
+  } catch (error) {
+    console.log("Could not get all routines");
+    throw error;
+  }
+  
 }
 
 async function getAllRoutinesByUser({username}) {
@@ -32,18 +46,29 @@ async function getPublicRoutinesByUser({username}) {
 }
 
 async function getAllPublicRoutines() {
-  const routines = await client.query(`
+  try {
+    const routines = await client.query(`
     SELECT *
     FROM routines
     WHERE "isPublic"=true;
   `)
+  return routines;
+
+  } catch (error) {
+    console.log("Could not get all public routines");
+    throw error;
+  }
+  
 }
 
 async function getPublicRoutinesByActivity({id}) {
+
 }
 
 async function createRoutine({creatorId, isPublic, name, goal}) {
-  await client.query(`
+  try {
+
+    await client.query(`
     INSERT INTO routines("creatorId", "isPublic", name, goal)
     VALUES ($1, $2, $3, $4)
     ON CONFLICT (name) DO NOTHING;
@@ -56,18 +81,31 @@ async function createRoutine({creatorId, isPublic, name, goal}) {
   `)
 
   return routine;
+
+  } catch (error) {
+    console.log("Could not complete 'createroutine'");
+    throw error;
+  }
+  
 }
 
 async function updateRoutine({id, ...fields}) {
 }
 
 async function destroyRoutine(id) {
-  const routines = await client.query(`
+  try {
+    const routines = await client.query(`
     DELETE FROM routines
     WHERE id=${id};
   `)
 
   return routines;
+
+  } catch (error) {
+    console.log("Could not delete routine");
+    throw error;
+  }
+  
 }
 
 module.exports = {
