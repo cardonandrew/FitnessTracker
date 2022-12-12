d9f749cd7414eba66151f4065ce6b7cdba89447d
Remics89 <Remics89@gmail.com>
2022-12-11 16:23:32 -0800

    tables dropped in order of dependency, added relative fields when creating tables

--- db/seedData.js
+++ db/seedData.js
@@ -7,10 +7,10 @@ async function dropTables() {
     console.log("Starting to drop tables...");
 
     await client.query(`
-      DROP TABLE IF EXISTS users;
-      DROP TABLE IF EXISTS activities;
-      DROP TABLE IF EXISTS routines;
       DROP TABLE IF EXISTS routine_activities;
+      DROP TABLE IF EXISTS routines;
+      DROP TABLE IF EXISTS activities;
+      DROP TABLE IF EXISTS users;
     `);
 
     console.log("Finished dropping tables!");
@@ -21,33 +21,49 @@ async function dropTables() {
 }
 
 async function createTables() {
+  console.log("Starting to build tables...")
   try {
-    console.log("Starting to build tables...");
+  
+      await client.query(`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username varchar(255) UNIQUE NOT NULL,
+          password varchar(255) NOT NULL
+          );
+        `);
 
-    await client.query(`
-      CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        await client.query(`
+          CREATE TABLE activities (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) UNIQUE NOT NULL,
+          description VARCHAR(255) NOT NULL
         );
-      CREATE TABLE activities (
-        id SERIAL PRIMARY KEY,
-        "authorId" INTEGER REFERENCES users(id)
-      );
-      CREATE TABLE routines (
-        id SERIAL PRIMARY KEY,
-        "authorId" INTEGER REFERENCES users(id)
-      );
-      CREATE TABLE routine-activities (
-        id SERIAL PRIMARY KEY,
-        "authorId" INTEGER REFERENCES users(id)
-      
-      );
-    
-      `);
-    } catch (error) {
-        console.error("Error building tables!");
-        throw error;}}
+        `);
+
+        await client.query(`
+        CREATE TABLE routines (
+          id SERIAL PRIMARY KEY,
+          "creatorId" INTEGER REFERENCES users(id) NOT NULL,
+          "isPublic" BOOLEAN DEFAULT TRUE,
+          name VARCHAR(255) UNIQUE NOT NULL,
+          goal VARCHAR(255) NOT NULL
+        );
+        `);
+
+        await client.query(`
+        CREATE TABLE "routine_activities" (
+          id SERIAL PRIMARY KEY,
+          "routineId" INTEGER REFERENCES routines(id) NOT NULL,
+          "activityId" INTEGER REFERENCES activities(id) NOT NULL,
+          count INTEGER NOT NULL,
+          duration INTEGER NOT NULL
+        );
+        `);
+  } catch (error) {
+    console.log("Could not build tables....");
+    throw error;
+ }
+}
 
 /* 
 
