78a9efae47fb42733cc52eeecfdc2470383f2ea1
cardonandrew <cardonandrew0@gmail.com>
2022-12-18 16:35:36 -0700

    feat: api/getAllPublicRoutines

--- api/routines.js
+++ api/routines.js
@@ -13,9 +13,25 @@ const {getRoutineById,
     destroyRoutine} = require('../db')
 
 // GET /api/routines
-
+routinesRouter.get('./routines', async (req, res, next) => {
+    const allPublicRoutines = await getAllPublicRoutines()
+    console.log('allRoutines:', allPublicRoutines)
+    res.send(allPublicRoutines)
+})
 // POST /api/routines
+routinesRouter.post('./routines', async (req, res, next) => {
+    const {creatorId, isPublic, name, goal} = req.body
 
+    if(!creatorId && !isPublic && !name && !goal){
+        next({name:"Missing Information",
+            message:"Please fill out all info"})
+    }
+
+    const newRoutine = await createRoutine(creatorId, isPublic, name, goal)
+    
+    res.send(newRoutine)
+
+})
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
